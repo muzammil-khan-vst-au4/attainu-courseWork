45b2e73515b7ecd17583805173f0c2cc77a04f2c
muzammil-khan-vst-au4 <muzammil.khan@outlook.in>
2019-10-01 10:12:43 +0530

    week04/day1/ all assignments completed

--- assignments/week04/day-1/assignment-1/index.js
+++ assignments/week04/day-1/assignment-1/index.js
@@ -1,32 +1,42 @@
 function getValue() {
     var value1 = parseInt(document.getElementById('value1').value);
     var value2 = parseInt(document.getElementById('value2').value);
-    console.log(value1,value2);
-    console.log(typeof(value1), typeof(value2));
 }
 
 var add = document.getElementById('add');
 add.addEventListener('click', function() {
-    getValue();
+  //  getValue();
+    var value1 = parseInt(document.getElementById('value1').value);
+    var value2 = parseInt(document.getElementById('value2').value);
     var result = (value1 + value2);
-    var  temp= document.getElementById('result');
-    temp.innerHTML = parseInt(result);
+    console.log(value1,value2);
+    console.log(result);
+    document.getElementById('result').innerHTML = result;
 });
 
 
 var subtract = document.getElementById('subtract');
 subtract.addEventListener('click', function() {
-    return value1 - value2;
+    var value1 = parseInt(document.getElementById('value1').value);
+    var value2 = parseInt(document.getElementById('value2').value);
+    var result = (value1 - value2);
+    document.getElementById('result').innerHTML = result;
 });
 
 
 var multiply = document.getElementById('multiply');
 multiply.addEventListener('click', function() {
-    return value1 * value2;
+    var value1 = parseInt(document.getElementById('value1').value);
+    var value2 = parseInt(document.getElementById('value2').value);
+    var result = (value1 * value2);
+    document.getElementById('result').innerHTML = result;
 });
 
 
 var divide = document.getElementById('divide');
 divide.addEventListener('click', function() {
-    return value1/value2;
+    var value1 = parseInt(document.getElementById('value1').value);
+    var value2 = parseInt(document.getElementById('value2').value);
+    var result = (value1 / value2);
+    document.getElementById('result').innerHTML = result;
 });
