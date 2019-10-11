46ad0966dd39558bc82051df6f943d4884539ab6
muzammil-khan-vst-au4 <muzammil.khan@outlook.in>
2019-10-11 11:46:55 +0530

    added condition for unequal string length

--- coding-challenges/week05/day-4/index.js
+++ coding-challenges/week05/day-4/index.js
@@ -6,11 +6,14 @@ var stringTwo = "This is string two";
 var hammingDistance = 0;
 console.log(stringOne);
 console.log(stringTwo);
-for(var i=0; i<stringOne.length; i++) {
-	if(stringOne[i]!==stringTwo[i]) {
-		hammingDistance++;
+if(stringOne.length === stringTwo.length) {
+	for(var i=0; i<stringOne.length; i++) {
+		if(stringOne[i]!==stringTwo[i]) {
+			hammingDistance++;
+		}
 	}
-
+}else {
+	console.log("Strigs not of equal length");
 }
 //alert("hamming distance is "+hammingDistance);
 console.log("hamming distance between string one and two is "+hammingDistance);
