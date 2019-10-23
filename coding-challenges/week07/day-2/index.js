ec85530fc1735e72c180ff96f466533a6bbdc16c
muzammil-khan-vst-au4 <muzammil.khan@outlook.in>
2019-10-23 11:56:13 +0530

    week07/day-2/coding challenge completed

---
+++ coding-challenges/week07/day-2/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const PORT = 9090;
+
+var nameArray = ["Ananya", "Anamika", "Nisha", "Nishant", "Nishank", "Ansal", "Gopi", "Arkesh" ,"Muzammil", "John", "Andy",
+                  "Cris", "Cristiano", "Marcel", "Marcelo", "James"];
+app.get('/', function(req, res) {
+    res.send('Hello! This is the homepage.');
+});
+
+app.get('/find', function(req, res){
+    var searchResult = [];
+    console.log("Query parameters >>>", req.query);
+    var findName = req.query.name;
+    nameArray.forEach(function(element) {
+        if(element.substring(0,findName.length) === findName) {
+            searchResult.push(element);
+        }
+    });
+    console.log(searchResult);
+    res.send(searchResult);
+});
+
+app.listen(PORT, function(){
+    console.log("App has started and running on port", PORT);
+});
