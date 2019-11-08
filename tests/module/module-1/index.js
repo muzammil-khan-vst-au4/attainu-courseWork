d13a2434b3dd34d80dc987651fac3df1a3426fad
muzammil-khan-vst-au4 <muzammil.khan@outlook.in>
2019-11-08 14:32:04 +0530

    view rendered

--- tests/module/module-1/index.js
+++ tests/module/module-1/index.js
@@ -26,6 +26,11 @@ MongoClient.connect(url, function(error, client) {
 	}
 	db = client.db('eagle-muzammil-khan');
 	collection = db.collection('users');
+	csv()
+	.fromFile(csvFilePath)
+	.then((jsonObj)=>{
+		insertdata(jsonObj);
+	})
 	console.log("Connection successfull");
 });
 
@@ -41,23 +46,73 @@ function insertdata(data){
 	})
 }
 
-app.get('/insert', function(req, res) {
-	csv()
-	.fromFile(csvFilePath)
-	.then((jsonObj)=>{
-		insertdata(jsonObj);
-	})
-})
-
 app.get('/users', function(req, res) {
 	collection.find().toArray(function(error, response) {
 		if(error){
-			console.log(error);
+			return res.send({
+				status: false,
+				message: error
+			});
 		}
-		return res.send(response);
+		return res.render('home',{
+			response
+		});
 	});
 })
 
+app.post('/users', function(req, res) {
+	var data = req.body;
+	collection.insertOne(data, function(error, response){
+		if(error){
+			return res.send({
+				status: false,
+				message: error
+			});
+		}
+		return res.send({
+			status: true,
+			message:"Successfully created user"
+		});
+	})
+})
+
+app.put('/users', function(req, res) {
+	var identifier = req.body.identifier;
+	var updateData = req.body.update;
+
+	collection.update({
+		name : identifier
+	}, {
+		"$set": updateData
+	}, function(error, response) {
+		if(error){
+			return res.send({
+				status: false,
+				message: error
+			});
+		}
+		return res.send({
+			status: true,
+			message:"Successfully updated user"
+		});
+	})
+})
+
+app.delete('/users', function(req, res) {
+	var identifier = req.body.identifier;
+	collection.deleteOne({ name : identifier }, function(err, result) {
+		if(error){
+			return res.send({
+				status: false,
+				message: error
+			});
+		}
+		return res.send({
+			status: true,
+			message:"Successfully deleted user"
+		});
+	})
+})
 
 
 
