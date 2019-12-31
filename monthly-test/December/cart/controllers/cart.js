177536bb346dd23222d323f37e4d78c282848ae2
Muzammil Khan <muzammil.khan@outlook.in>
2019-12-31 14:02:40 +0530

    temp

--- monthly-test/December/cart/controllers/cart.js
+++ monthly-test/December/cart/controllers/cart.js
@@ -1,13 +1,22 @@
+const Cart = require('./../models/index').Cart
+//const CartArray = []
 class Cart {
-    constructor() {
-
-    }
-
-    static addItem() {
-
-    }
-    deleteItem() {  
-
+    static addItem(itemName, quantity, pricePerItem) {
+        const newItem = {
+            itemName: itemName,
+            quantity: quantity,
+            pricePerItem: pricePerItem,
+            amount: quantity*pricePerItem
+        }
+        //CartArray.push(newItem)
+        Cart.create({items: newItem}, function(err, res) {
+            if(error) {
+				console.log(err);
+				return res.send(err)
+			}
+			return res.send("item added")
+        }) 
     }
 }
+
 module.exports = Cart;
