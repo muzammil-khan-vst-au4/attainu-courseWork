51420851519024439db3378cb26f609d0e74de07
muzammil-khan-vst-au4 <muzammil.khan@outlook.in>
2020-01-15 23:26:05 +0530

    week19 day-3 assigment completed

--- coding-challenges/week19/day-3/reverseLL.js
+++ coding-challenges/week19/day-3/reverseLL.js
@@ -40,6 +40,20 @@ class SinglyLinkedList{
         }
         return current;
     }
+    reverse(){
+      var node = this.head;
+      this.head = this.tail;
+      this.tail = node;
+      var next;
+      var prev = null;
+      for(var i = 0; i < this.length; i++){
+        next = node.next;
+        node.next = prev;
+        prev = node;
+        node = next;
+      }
+      return this;
+    }
     print(){
         var arr = [];
         var current = this.head
